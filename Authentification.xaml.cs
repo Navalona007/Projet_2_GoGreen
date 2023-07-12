82cc8b7d6bb35313998745783c2ac22dc5f9c053
soava <soavamahary@gmail.com>
2023-07-12 18:39:00 +0300

    connexion base de donnée authentification; verification login et Mdp

--- Authentification.xaml.cs
+++ Authentification.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Npgsql;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,13 +21,17 @@ namespace Projet_2_GoGreen
     /// </summary>
     public partial class Authentification : Window
     {
-        NpgsqlConnection conx;
-
+        NpgsqlConnection conn = new NpgsqlConnection("Host=localhost; Port=5432; Database=gg_db;Username=postgres;Password=1234");
         public Authentification()
         {
             InitializeComponent();
-            conx = new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=1234;Database=gg_db;");
 
+        }
+
+
+        public static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=1234;Database=gg_db");
         }
         string login;
         string password;
@@ -34,13 +39,64 @@ namespace Projet_2_GoGreen
         string defaultPassword = "admin";
 
 
-
         private void bt_connect_Click(object sender, RoutedEventArgs e)
-        {            
-            login = tb_login.Text.ToString();
-            password = pwd_auth.Password;
-            CompareLoginPassword(); 
+        {
+
+            if (!string.IsNullOrEmpty(tb_login.Text) || !string.IsNullOrEmpty(pwd_auth.Password))
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection("Host=localhost;Database=gg_db;Username=postgres;Password=1234"))
+                {
+                    conn.Open();
+
+                    // Vérifier si l'utilisateur est un administrateur
+                    using (NpgsqlCommand adminCmd = new NpgsqlCommand("SELECT mail_admin, pass_admin FROM administrateur WHERE mail_admin = '" + tb_login.Text + "' AND pass_admin = '" + pwd_auth.Password + "'", conn))
+                    {
+                        adminCmd.Parameters.AddWithValue("@mail_admin", tb_login.Text);
+                        adminCmd.Parameters.AddWithValue("@pass_admin", pwd_auth.Password);
+
+                        using (var adminReader = adminCmd.ExecuteReader())
+                        {
+                            if (adminReader.Read())
+                            {
+                                Administrateur administrateur = new Administrateur();
+                                administrateur.Show();
+                                //MessageBox.Show("Mety", "Ok");
+                                tb_login.Text = "";
+                                pwd_auth.Password = "";
+                                return;
+                            }
+                        }
+                    }
+
+                    // Vérifier si l'utilisateur est un client
+                    using (NpgsqlCommand ClientCmd = new NpgsqlCommand("SELECT mail_client, pass_client FROM client WHERE mail_client = '" + tb_login.Text + "' AND pass_client = '" + pwd_auth.Password + "'", conn))
+                    {
+                        ClientCmd.Parameters.AddWithValue("@mail_client", tb_login.Text);
+                        ClientCmd.Parameters.AddWithValue("@pass_client", pwd_auth.Password);
+
+                        using (var clientReader = ClientCmd.ExecuteReader())
+                        {
+                            if (clientReader.Read())
+                            {
+                                //Administrateur administrateur = new Administrateur();
+                                //administrateur.Show();
+                                MessageBox.Show("Bienvenu sur votre plateforme", "Reussi");
+                                tb_login.Text = "";
+                                pwd_auth.Password = "";
+                                return;
+                            }
+                        }
+                    }
+                    // Si aucun utilisateur correspondant n'est trouvé
+                    MessageBox.Show("Votre identifiant et mot de passe ne correspondent pas. Réessayez", "Erreur");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez remplir tous les champs de connexion", "Erreur");
+            }
         }
+
 
 
         public void CompareLoginPassword()
@@ -62,7 +118,7 @@ namespace Projet_2_GoGreen
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+
         }
 
         private void tb_login_TextChanged(object sender, TextChangedEventArgs e)
@@ -70,9 +126,38 @@ namespace Projet_2_GoGreen
 
         }
 
-        
-            
-            
 
+        // Fonction pour calculer le hachage MD5 d'une chaîne de caractères
+        static string CalculateMD5Hash(string input)
+        {
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+                System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        } //end CalculateMD5Hash
+
+        //private bool VerifyPassword(string email, string password)
+        //{
+        //    // Récupérer le mot de passe haché depuis la base de données en fonction de l'email
+        //    // ...
+        //    using (NpgsqlConnection conn = new NpgsqlConnection("Host=localhost;Database=gg_db;Username=postgres;Password=1234"))
+        //    {
+        //        conn.Open();
+
+        //        // Calculer le haché MD5 du mot de passe saisi
+        //        string hashedPasswordToVerify = CalculateMD5Hash(password);
+
+        //        // Comparer les deux hachés de mot de passe
+        //        return hashedPasswordFromDatabase.Equals(hashedPasswordToVerify);
+        //    }//verifyPassword
+
+        //}
     }
 }
