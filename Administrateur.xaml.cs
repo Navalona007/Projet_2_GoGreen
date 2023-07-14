d8ba3c81055def61eb562de4e6831e4c5ce479a3
Murielle <muriellesteffi@gmail.com>
2023-07-14 18:13:29 +0300

    Interface Authentification, interface test_op, interface authentification, connection database, lecture et ecriture données database dans datagridview operateur, ajustement de la taille datagridview operateur, Classe Operateur_class : get set

--- Administrateur.xaml.cs
+++ Administrateur.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Npgsql;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,9 +26,20 @@ namespace Projet_2_GoGreen
         public Administrateur()
         {
             InitializeComponent();
+            lecture_ecriture();
         }
+        
         private void bt_cancel_oper_Click(object sender, RoutedEventArgs e)
         {
+            tb_nom_oper.Text = "";
+            tb_prenom_oper.Text = "";
+            tb_email_oper.Text = "";
+            tb_lieu.Text = "";
+            tb_mobile_oper.Text = "";
+            pwd_oper.Password = "";
+            pwd_oper.Password = "";
+
+
             Authentification authentification = new Authentification();
             authentification.Show();
             this.Hide();
@@ -60,7 +73,7 @@ namespace Projet_2_GoGreen
         }
         private void connectDB()
         {
-            NpgsqlConnection conx = new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=a1234;Database=gg_db;");
+            NpgsqlConnection conx = new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=root;Database=gg_db;");
             conx.Open();
             if (conx.State == System.Data.ConnectionState.Open)
             {
@@ -80,6 +93,7 @@ namespace Projet_2_GoGreen
             {
                 connectDB();
 
+
             }
             catch (Exception ex)
             {
@@ -89,7 +103,7 @@ namespace Projet_2_GoGreen
 
         private void bt_modifier_oper_Click(object sender, RoutedEventArgs e)
         {
-
+           
         }
 
         private void tb_search_client_TextChanged(object sender, TextChangedEventArgs e)
@@ -123,5 +137,89 @@ namespace Projet_2_GoGreen
             authentification.Show();
             this.Hide();
         }
+
+        private void lecture_ecriture()
+        {
+            ObservableCollection<OperateurClass> listeOperateurs = new ObservableCollection<OperateurClass>();
+            NpgsqlConnection GetConnection()
+            {
+                return new NpgsqlConnection("Server=localhost;Port=5432;User Id=postgres;Password=root;Database=gg_db");
+            }
+            try
+            {
+                var conn = GetConnection();
+                conn.Open();
+
+                String query = "SELECT opérateur_de_saisi.id, nom_oper, prenom_oper, mail_oper, mobile_oper, name_lieu, status " +
+                                "FROM opérateur_de_saisi " +
+                                "INNER JOIN lieu_travail ON opérateur_de_saisi.lieu_travailid = lieu_travail.id " +
+                                "INNER JOIN statut_opérateur ON opérateur_de_saisi.statut_opérateurid = statut_opérateur.id";
+                NpgsqlCommand command = new NpgsqlCommand(query, conn);
+
+                //List<OperateurClass> listeOperateurs = new List<OperateurClass>();
+                
+
+                 BindingList<OperateurClass> bindingList = new BindingList<OperateurClass>(listeOperateurs);
+
+                NpgsqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    OperateurClass operateur = new OperateurClass();
+
+                     grid_oper.ItemsSource = bindingList;
+
+                    operateur.setId(reader["id"].ToString());
+                    operateur.setName(reader["nom_oper"].ToString());
+                    operateur.setLastname(reader.GetString(reader.GetOrdinal("prenom_oper")));
+                    operateur.setEmail(reader.GetString(reader.GetOrdinal("mail_oper")));
+                    operateur.setMobile(reader.GetString(reader.GetOrdinal("mobile_oper")));
+                    operateur.setWorkplace(reader.GetString(reader.GetOrdinal("name_lieu")));
+                    operateur.setStatut(reader.GetString(reader.GetOrdinal("status")));
+
+                    listeOperateurs.Add(operateur);
+
+                }
+                grid_oper.ItemsSource = listeOperateurs;
+                
+                conn.Close();
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "erreur request", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        
+        //public string lecture_lieu()
+        //{
+        //    NpgsqlConnection GetConnection()
+        //    {
+        //        return new NpgsqlConnection("Server=localhost;Port=5432;User Id=postgres;Password=root;Database=gg_db");
+        //    }
+        //    var conn = GetConnection();
+        //    conn.Open();
+
+        //    int id = 1; 
+        //    //string nomlieu = "nom"; // Le nom de la colonne que vous souhaitez récupérer
+
+        //    string query = "SELECT nom_lieu FROM lieu_travail WHERE ID = id";
+        //    using (NpgsqlCommand command = new NpgsqlCommand(query, conn))
+        //    {
+        //        command.Parameters.AddWithValue("id", id);
+
+        //        using (NpgsqlDataReader reader = command.ExecuteReader())
+        //        {
+        //            while (reader.Read())
+        //            {
+
+        //                string columnValue = reader.GetString(0); // Récupère la valeur de la colonne
+
+ 
+  
+        //            }
+        //        }
+        //        //String query = "SELECT name_lieu FROM lieu_travail WHERE ID = id";
+        //        return null;
+        //}
     }
 }
--- OperateurClass.cs
+++ OperateurClass.cs
@@ -1,5 +1,142 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Collections.ObjectModel;
+//using System.ComponentModel;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Projet_2_GoGreen
+//{
+//    internal class OperateurClass
+//    {
+//        private string name;
+//        private string lastname;
+//        private string workplace;
+//        private string email;
+//        private string password;
+//        private string mobile;
+
+//        ConnectDB conx = new ConnectDB();
+
+//        public static List<OperateurClass> list_operateur = new List<OperateurClass>();
+//        public static Dictionary<String, OperateurClass> list_pass_operateur = new Dictionary<string, OperateurClass>();
+//        public static Dictionary<String, OperateurClass> list_login_operateur = new Dictionary<string, OperateurClass>();
+
+//        public OperateurClass(string name, string lastname, string email, string password, string workplace)
+//        {
+//            this.name = name;
+//            this.lastname = lastname;
+//            this.email = email;
+//            this.password = password;
+//            this.workplace = workplace;
+
+//        }
+//        public string getWorkplace()
+//        {
+//            return workplace;
+//        }
+//        public void setWorkplace(string value)
+//        {
+//            this.workplace = value;
+//        }
+//            public string getName()
+//        {
+//            return name;
+//        }
+
+//            public void setName (string value)
+//        {
+//            this.name = value;
+//        }
+//        public string getLastname()
+//        {
+//            return lastname;
+//        }
+
+//        public void setLastname(string value)
+//        {
+//            this.lastname = value;
+//        }
+//        public string getEmail()
+//        {
+//            return email;
+//        }
+
+//        public void setEmail(string value)
+//        {
+//            this.email = value;
+//        }
+//        public string getPassword()
+//        {
+//            return password;
+//        }
+
+//        public void setPassword(string value)
+//        {
+//            this.password = value;
+//        }
+
+//        public OperateurClass()
+//        {
+
+//        }
+
+//        private void liste_operateur()
+//        {
+//            var cmd = conx.createRequest("Select * from opérateur_de_saisi;");
+//            var read = cmd.ExecuteReader();
+
+//            while (read.Read())
+//            {
+//                string mail = (string)read["mail_oper"];
+//                string pass = (string)read["pass_oper"];
+//                string nom = (string)read["nom_oper"];
+//                string prenom = (string)read["prenom_oper"];
+//                string workplace = (string)read["mobile_oper"];
+//                list_pass_operateur.Add(pass, new OperateurClass(nom, prenom, mail, pass, workplace));
+//                list_login_operateur.Add(mail, new OperateurClass(nom, prenom, mail, pass, workplace));
+//            }
+//        }
+
+//        //private void lecture_ecriture()
+//        //{
+
+
+//        //    String query = "SELECT * FROM opérateur_de_saisi";
+
+//        //    var cmd = conx.createRequest(query);
+//        //    var reader = cmd.ExecuteReader();
+
+//        //    //List<OperateurClass> listeOperateurs = new List<OperateurClass>();
+//        //    ObservableCollection<OperateurClass> listeOperateurs = new ObservableCollection<OperateurClass>();
+
+//        //    //NpgsqlDataReader reader = command.ExecuteReader();
+//        //    while (reader.Read())
+//        //    {
+//        //        OperateurClass operateur = new OperateurClass();
+
+//        //        BindingList<OperateurClass> bindingList = new BindingList<OperateurClass>(listeOperateurs);
+//        //        grid_oper.DataSource = bindingList;
+
+//        //        operateur.setName(reader.GetString(reader.GetOrdinal("nom_oper")));
+//        //        operateur.setLastname(reader.GetString(reader.GetOrdinal("prenom_oper")));
+//        //        operateur.setEmail(reader.GetString(reader.GetOrdinal("mail_oper")));
+//        //        operateur.setWorkplace(reader.GetString(reader.GetOrdinal("mobile_oper")));
+
+//        //        listeOperateurs.Add(operateur);
+
+//        //    }
+//        //    grid_oper.ItemsSource(listeOperateurs);
+//        //    reader.Close();
+//        //}
+//    }
+//}
+
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +145,23 @@ namespace Projet_2_GoGreen
 {
     internal class OperateurClass
     {
-        private string name;
-        private string lastname;
-        //private string workplace;
-        private string email;
-        private string password;
-        private string mobile;
+        public string id { get; set; }
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public string workplace { get; set; }
+        public string email { get; set; }
+        public string mobile { get; set; }
+        public string statut { get; set; }
+
+        public void setWorkplace(string v)
+        {
+            workplace = v;
+        }
+
+        public void setStatut(string v)
+        {
+            statut = v;
+        }
 
         ConnectDB conx = new ConnectDB();
 
@@ -21,14 +169,66 @@ namespace Projet_2_GoGreen
         public static Dictionary<String, OperateurClass> list_pass_operateur = new Dictionary<string, OperateurClass>();
         public static Dictionary<String, OperateurClass> list_login_operateur = new Dictionary<string, OperateurClass>();
 
-        public OperateurClass(String name, string lastname, string email, string password)
+        public OperateurClass(string id, string name, string lastname, string email, string password, string workplace)
         {
             this.name = name;
             this.lastname = lastname;
             this.email = email;
-            this.password = password;
-            //this.workplace = workplace;
+           
+            this.workplace = workplace;
         }
+
+        public string getWorkplace()
+        {
+            return workplace;
+        }
+
+        public void setMobile(string value)
+        {
+            mobile = value;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getId()
+        {
+            return id;
+        }
+
+        public void setId(string value)
+        {
+            id = value;
+        }
+
+        public void setName(string value)
+        {
+            name = value;
+        }
+
+        public string getLastname()
+        {
+            return lastname;
+        }
+
+        public void setLastname(string value)
+        {
+            lastname = value;
+        }
+
+        public string getEmail()
+        {
+            return email;
+        }
+
+        public void setEmail(string value)
+        {
+            email = value;
+        }
+
+
         public OperateurClass()
         {
 
@@ -45,10 +245,27 @@ namespace Projet_2_GoGreen
                 string pass = (string)read["pass_oper"];
                 string nom = (string)read["nom_oper"];
                 string prenom = (string)read["prenom_oper"];
-                //string 
-                list_pass_operateur.Add(pass, new OperateurClass(nom, prenom, mail, pass));
-                list_login_operateur.Add(mail, new OperateurClass(nom, prenom, mail, pass));
+                string workplace = (string)read["mobile_oper"];
+                list_pass_operateur.Add(pass, new OperateurClass(id,nom, prenom, mail, pass, workplace));
+                list_login_operateur.Add(mail, new OperateurClass(id,nom, prenom, mail, pass, workplace));
             }
+        }
+
+        // Code for creating DataGridTextColumns
+
+        public static ObservableCollection<OperateurClass> GetOperateurs()
+        {
+            ObservableCollection<OperateurClass> listeOperateurs = new ObservableCollection<OperateurClass>();
+
+            // Retrieve data from your data source
+            // For example, fetch data from list_operateur or list_pass_operateur dictionaries
+
+            foreach (var operateur in list_operateur)
+            {
+                listeOperateurs.Add(operateur);
+            }
+
+            return listeOperateurs;
         }
     }
 }
